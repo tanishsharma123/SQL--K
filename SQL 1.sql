eb5d38045c11bcc0e27dabcf026b6a015cbd05d0
tanishsharma123 <tanish829017@gmail.com>
2025-07-22 11:31:33 +0530

    SQL-1

---
+++ SQL 1.sql
@@ -0,0 +1,36 @@
+# what is data?
+# ==> 
+# what is diff between data and raw data?
+# ==> Data is a collection of meaning full data and raw data is collection of unfilltered data,unprocezes
+# data base is a place to store data.
+
+# ==> how to defined data in big data/ data amout in large or terabyte?
+# v---> volume -- storage/size of data
+# v---> velocity-- ret of generation of data 
+# v---> variety-- types of data
+# v--->veracity-- trustworthiness of your data
+# v---> values --meaning
+
+# ===> ER Diagram ---> Entity relational diagram
+
+# every real world object is called Entity.
+# when we have an student name table/entity show by rectangle
+# some colume like - id, name, age etc are know as attribute
+# And rows are known as touple.
+# Relation show as diamond
+
+# DECCRIBE THE PROPERTY OF ATTRIBUTE
+
+# =====> TYPE OF  
+# 1. STRONG ENTITY
+# 2 .WEAK ENTITY
+
+
+
+# ======>
+
+
+show databases;
+use regex;
+use sakila;
+describe actor;
